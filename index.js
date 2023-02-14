f35c674e3b4c5d77f07b8f3485880864dd8f3457
Rohit Ingole <ingolerohit2017@gmail.com>
2023-02-14 11:33:56 +0530

    javascript if else ladder

---
+++ index.js
@@ -0,0 +1,21 @@
+var goli = false;
+var biscuit = true;
+var biryani = true;
+var mess = true;
+if (goli){
+    console.log("buy goli");
+}
+else if(biscuit){
+    console.log("buy biscuit");
+
+}
+else if ( biryani ){
+    console.log("buy biryani");
+
+}
+else if (mess){
+    console.log("buy mess");
+}
+else{
+    console.log("mar sale kutte");
+}
